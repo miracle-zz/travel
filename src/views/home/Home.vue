62be56341f72ac65b65254ca228ee06102d04c6f
miracleZ <799720991@qq.com>
2020-08-14 18:25:53 +0800

    index-recommend

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,18 +2,39 @@
   <div>
     <home-header></home-header>
     <carrousel></carrousel>
+    <home-icons></home-icons>
+    <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import carrousel from './components/Swiper'
+import HomeIcons from './components/HomeIcons'
+import HomeRecommend from './components/HomeRecommend'
+import HomeWeekend from './components/HomeWeekend'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    carrousel
+    carrousel,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
   }
 }
 </script>
---
+++ src/views/home/components/HomeIcons.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">热门标签</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          title: '景点门票',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+        },
+        {
+          title: '一日游',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png'
+        },
+        {
+          title: '本地玩乐',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/f5/a963333e1fa802.png'
+        },
+        {
+          title: '动植物园',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png'
+        },
+        {
+          title: '必游榜单',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png'
+        },
+        {
+          title: '自然风光',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png'
+        },
+        {
+          title: '海滨海岛',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/27/dac2bcf9added702.png'
+        },
+        {
+          title: '踏青赏花',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png'
+        },
+        {
+          title: '城市观光',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png'
+        },
+        {
+          title: '全部玩乐',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icons {
+  margin-top: 0.2rem;
+}
+.icons >>> .swiper-container {
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icon {
+  position: relative;
+  overflow: hidden;
+  float: left;
+  width: 25%;
+  height: 0;
+  padding-bottom: 25%;
+}
+
+.icon-img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  box-sizing: border-box;
+  padding: 0.1rem;
+  bottom: 0.44rem;
+}
+.icon-img-content {
+  display: block;
+  margin: 0 auto;
+  height: 100%;
+}
+
+.icon-desc {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 0.44rem;
+  text-align: center;
+  line-height: 0.44rem;
+  color: var(--color-darkText);
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
